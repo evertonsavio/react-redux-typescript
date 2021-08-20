fdb04f4d54e3bf9b7f028164bedb350fcda01dba
Rhavyx <everluca@outlook.com>
2021-08-20 13:16:26 -0300

    :sparkles: action interfaces

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,15 +4,29 @@ interface RepositoriesState {
     data: string[];
 }
 
-interface Action {
+/* interface Action {
     type: string;
     payload?: any;
+} */
+
+interface SearchRepositoriesAction{
+    type: 'search_repositories';
+}
+interface SearchRepositoriesSuccessAction{
+    type: 'search_repositories_success';
+    payload: string[];
+}
+interface SearchRepositoriesErrorAction{
+    type: 'search_repositories_error';
+    payload: string;
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+const reducer = (state: RepositoriesState, action: | SearchRepositoriesAction | SearchRepositoriesSuccessAction 
+    | SearchRepositoriesErrorAction): RepositoriesState => {
+
     switch (action.type) {
         case 'search_repositories':
-            return {loading: true, error: null, data: []}
+            return { loading: true, error: null, data: []}
         case 'search_repositories_success':
             return { loading: false, error: null, data: action.payload} 
         case 'search_repositories_error':
