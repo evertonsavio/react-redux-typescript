1c20982a3b28a538cc8023be99dd76a061cc09de
Rhavyx <everluca@outlook.com>
2021-08-20 14:43:51 -0300

    :rocket: redux & typescript together

--- src/components/repositoriesList.tsx
+++ src/components/repositoriesList.tsx
@@ -1,23 +1,17 @@
 import { useState } from "react";
-/* V2 import { useSelector } from "react-redux"; */
 import { useTypedSelector } from "../hooks/useTypedSelection";
-/* V1 import { useDispatch } from "react-redux";
-import { actionCreators } from "../state"; */
 import { useActions } from "../hooks/useAction";
 
 const RepositoriesList: React.FC = () => {
 
     const [term, setTerm] = useState('');
-    // V1 const dispatch = useDispatch();
     const { searchRepositories } = useActions();
-    // V2 const {data, error, loading} = useSelector((state: any) => state.repositories)
     const {data, error, loading} = useTypedSelector(
         (state) => state.repositories
     );
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        //V1 dispatch(actionCreators.searchRepositories(term))
         searchRepositories(term);
     };
 
@@ -27,6 +21,10 @@ const RepositoriesList: React.FC = () => {
             <input value={term} onChange={e => setTerm(e.target.value)}/>
             <button>Search</button>
         </form>
+        {error && <h3>{error}</h3>}
+        {loading && <h3>Loading...</h3>}
+        {!error && !loading && 
+            data.map((name) => <div key={name}>{name}</div>)}
 
     </div>
 }
