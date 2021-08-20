25818119b68cefc8434aa4daf0a5fe6c1786ca50
Rhavyx <everluca@outlook.com>
2021-08-20 13:57:15 -0300

    :sparkles: search react component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello</div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
